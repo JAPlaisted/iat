88b5cff54a0794efcc2590c9af4c6030b5f5c1e2
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-07-21 13:13:12 -0700

    New hero image & design

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,7 +5,7 @@ import "../css/hero.css";
 const Hero = () => {
   return (
     <section className="hero">
-        <p className="tagline animate__animated animate__backInLeft">boring jobs <br /> are for <br /> <span className="primary-text">robots</span></p>
+        <p className="tagline animate__animated animate__backInLeft">boring jobs <br /> are for <br /> <span >robots</span></p>
     </section>
   );
 };
