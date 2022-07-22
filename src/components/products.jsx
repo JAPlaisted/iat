c6713c3e68f7bbeb1b83320d474f6207c1ae2f49
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-07-21 17:37:10 -0700

    Content block created

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -3,6 +3,29 @@ import "../css/products.css";
 
 
 const Products = () => {
+
+    const texts = ["despair", "prosper", "eat cake"];
+    let count = 0;
+    let index = 0;
+    let currentText = "";
+    let letter = "";
+
+    (function type() {
+     if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector(".typing").textContent = letter;
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 300);
+})();
+
+
   return (
     <section className="products">
         <div className="grid grid--1x3">
@@ -13,8 +36,13 @@ const Products = () => {
             </div>
             <h4>Explore</h4>
             </div>
-            <div className="grid--block communication--block"><h3>Mass Communication</h3></div>
-            <div className="grid--block content--block"><h3>Content Creation</h3></div>
+            <div className="grid--block communication--block"><h3>Mass Communication</h3>
+            <h2>Send thousands of <br/> 
+            targeted emails</h2>
+            <h4>Explore</h4></div>
+            <div className="grid--block content--block"><h3>Content Creation</h3>
+            <p>"Look on  my works <br/> ye mighty, and <br/> <strong class="typing"></strong>!"</p>
+            <h4>Explore</h4></div>
         </div>
     </section>
   );
