7cc4cbd19e32f1b241a3eae0d063592bdee91fc3
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-07-26 12:47:56 -0700

    Content creation block made

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -13,8 +13,13 @@ const Products = () => {
         <div className="grid grid--1x3">
             <div className="grid--block virtual--block"><h3>Virtual Customer Service</h3>
             <div className="bot">
-                <img src="https://randomuser.me/api/portraits/women/79.jpg" alt="Random User" />
-                <h3>Hi, I'm Stacy. How can I help you?</h3>
+                <div className="random--user"><img src="https://randomuser.me/api/portraits/women/79.jpg" alt="Random User" />
+                <h3>Stacy</h3>
+                <div className="green--circle"></div>
+                </div>
+                <p className="message--bot">Hi, I'm Stacy. How can I help you?</p>
+                <p className="message--user">Hi, I was hoping to get <br/> a quote on something.</p>
+                <p className="message--bot">Sure, what are you looking to get a quote on?</p>
             </div>
             <h4>Explore</h4>
             </div>
@@ -31,7 +36,15 @@ const Products = () => {
             </div>
             <h4>Explore</h4></div>
             <div className="grid--block content--block"><h3>Content Creation</h3>
-            <p>"Look on  my works <br/> ye mighty, and <br/> <strong class="typing"></strong>!"</p>
+            <div className="blog--post">
+                <div className="blog--header"></div>
+                <div className="blog--body">8 Companies to Follow in 2023</div>
+                <div className="blog--footer">
+                    <img src="https://randomuser.me/api/portraits/men/86.jpg" alt="Random User" />
+                    <p>Troy Young, <br/>
+                    Mar 19, 2022</p>
+                </div>
+            </div>
             <h4>Explore</h4></div>
         </div>
     </section>
