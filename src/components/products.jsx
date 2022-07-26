07f13aedcce130c96f64d1c83a033ceef0079af1
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-07-25 19:02:32 -0700

    Mass comm block updated

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -4,7 +4,9 @@ import "../css/products.css";
 
 const Products = () => {
 
-
+    const handleSubmit = event => {
+        event.preventDefault();
+    };
 
   return (
     <section className="products">
@@ -17,8 +19,16 @@ const Products = () => {
             <h4>Explore</h4>
             </div>
             <div className="grid--block communication--block"><h3>Mass Communication</h3>
-            <h2>Send thousands of <br/> 
-            targeted emails</h2>
+            <div className="email">
+                <h3 className="email--header">New Message</h3>
+                <form onSubmit={handleSubmit}>
+                <input type="text" id="recipients" name="recipients" placeholder="All Businesses in San Francisco"/>
+                <input type="text" id="fname" name="fname" placeholder="Catering Services Offer"/>
+                <textarea name="input-text" id="input-text" cols="30" rows="5" placeholder="Send thousands of targeted emails in just a few clicks!">
+                </textarea> <br/>
+                <input className="btn" type="submit" value="Send"/>
+                </form>
+            </div>
             <h4>Explore</h4></div>
             <div className="grid--block content--block"><h3>Content Creation</h3>
             <p>"Look on  my works <br/> ye mighty, and <br/> <strong class="typing"></strong>!"</p>
