3088fe1c3db96813230b9cf79c8492d8e7360cc4
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-07-28 16:22:53 -0700

    Products block styling additions

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -39,7 +39,7 @@ const Products = () => {
             <div className="grid--block content--block"><h3>Content Creation</h3>
             <div className="blog--post" data-aos="zoom-in">
                 <div className="blog--header"></div>
-                <div className="blog--body">8 Companies to Follow in 2023</div>
+                <div className="blog--body"><h4>8 Companies to Follow in 2023</h4><p>These eight companies have been changing the way I think about business. From disrupting long-standing industries to finding new markets among consumers, these companies are worth learning more about to inspire your own entrepreneurial efforts.</p></div>
                 <div className="blog--footer">
                     <img src="https://randomuser.me/api/portraits/men/86.jpg" alt="Random User" />
                     <p>Troy Young, <br/>
