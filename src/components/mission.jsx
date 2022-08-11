568c5961c7bfad1e897ecfd5c1a4b71766d2f46e
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2022-08-10 19:14:47 -0700

    Mission link

--- src/components/mission.jsx
+++ src/components/mission.jsx
@@ -5,9 +5,11 @@ import "../css/mission.css";
 const Mission = () => {
   return (
     <section className="mission">
-      <h3 data-aos="fade-right">Build a better <br/>
-      future <br/>
-      with us.<br/></h3>
+      <h3 data-aos="fade-right"><a href="/solutions">
+        Build a better <br/>
+        future <br/>
+        with us.<br/>
+      </a></h3>
       <div className="city"></div>
     </section>
   );
